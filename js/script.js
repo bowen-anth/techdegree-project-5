622417ecf0e9ea7ac6490f093dcae79fd2d1b3cf
Anthony Bowen <bowen.anth@gmail.com>
2020-06-30 15:19:52 -0700

    edited js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,39 @@ lightbox.option({
   'wrapAround': true
 });
 
+const list = document.querySelector('div');
+const searchField = document.querySelector('#photo-search');
+searchField.addEventListener('keyup', function(eventObject){
+  const term = eventObject.target.value.toLowerCase();
+  const images = list.getElementsByTagName('a');
+  Array.from(images).forEach(function(image){
+    //I think the title const is messing it up?
+    const title = image.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(term) != -1){
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+      }
+  });
+});
+
+/* function mySearchFunction() {
+  var input, filter, a, item, i, txtValue;
+  input = document.getElementById("photo-search");
+  filter = input.value.toLowerCase();
+  mainDiv = document.getElementByClassName("main-div");
+  a = mainDiv.getElementsByTagName("a");  for (i = 0; i < li.length; i++) {
+    item = a[i];
+    txtValue = item.textContent || item.innerText;
+    if (txtValue.toLowerCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+*/
+
 /* const search = document.querySelector('#photo-search');
 const boxTexts = document.querySelectorAll('.main-div a');
 
@@ -26,20 +59,6 @@ search.addEventListener('keyup', handleSearch);
 
 
 // search
-const list = document.querySelector('div');
-const searchField = document.querySelector('#photo-search');
-searchField.addEventListener('keyup', function(eventObject){
-  const term = eventObject.target.value.toLowerCase();
-  const images = list.getElementsByTagName('a');
-  Array.from(images).forEach(function(image){
-    const title = image.firstElementChild.textContent;
-    if(title.toLowerCase().indexOf(term) != -1){
-      image.style.display = 'block';
-    } else {
-      image.style.display = 'none';
-      }
-  });
-});
 
 /* document.getElementById("photo-search").addEventListener("keyup", myFunction);
 
